2c4c20ef5302e78a0ed7e3a97c4678ccbb925ec1
Talleeenos69 <106496088+Talleeenos69@users.noreply.github.com>
2023-05-18 21:59:54 -0400

    stuff
    first commit woooo!!!

---
+++ website/auth.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, render_template
+
+auth = Blueprint("auth", __name__)
+
+
+@auth.route("/login")
+def login():
+    return "login"
+
+@auth.route("/signup")
+def signup():
+    return "signup"
+
+@auth.route("/logout")
+def logout():
+    return "Logout"
